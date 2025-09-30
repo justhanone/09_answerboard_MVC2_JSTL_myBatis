567739b6f82f74241917569ef4c7eccaee19b4a5
justhanone <168494983+justhanone@users.noreply.github.com>
2025-09-30 09:14:51 +0900

    Initial commit

---
+++ src/main/java/com/hk/board/dao/AnsDao.java
@@ -0,0 +1,153 @@
+package com.hk.board.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.hk.board.config.SqlMapConfig;
+import com.hk.board.dto.AnsDto;
+
+public class AnsDao extends SqlMapConfig{
+
+	private String namespace="com.hk.board.dao.";
+	
+	//1.글목록 조회
+	public List<AnsDto> getAllList(){
+		List<AnsDto> list=new ArrayList<AnsDto>();
+		SqlSession sqlSession=null;
+		
+		try {
+			//sqlSession객체를 구하려면 openSession()를 통해 얻어온다
+			//openSession(true/false): t(autocommit)
+			sqlSession=getSessionFactory().openSession(true);
+			// selectList(쿼리ID) 해당 쿼리를 실행시킨다.
+			list=sqlSession.selectList(namespace+"boardlist");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return list;
+	}
+	
+	//2. 글추가하기
+	public boolean insertBoard(AnsDto dto){
+		int count=0;
+		SqlSession sqlSession=null;
+		
+		try {
+			sqlSession=getSessionFactory().openSession(true);
+			//파라미터 타입: dto, Array, Map, String, Integer 등
+			count=sqlSession.insert(namespace+"insertboard",dto);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return count>0?true:false;
+	}
+	
+	//3.글상세 조회
+	public AnsDto getBoard(int seq){
+		AnsDto dto=new AnsDto();
+		SqlSession sqlSession=null;
+		//파라미터 전달 방식의 기본은 map에 담아서 전달한다.
+//		Map<String, Integer>map=new HashMap<String, Integer>();
+//		map.put("seq", seq);
+		try {
+			sqlSession=getSessionFactory().openSession(true);
+			dto=sqlSession.selectOne(namespace+"getboard",seq);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return dto;
+	}
+	
+	//4. 조회수 올리기
+	public boolean readCount(int seq){
+		int count=0;
+		SqlSession sqlSession=null;
+		
+		try {
+			sqlSession=getSessionFactory().openSession(true);
+			//파라미터 타입: dto, Array, Map, String, Integer 등
+			count=sqlSession.update(namespace+"readcount",seq);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return count>0?true:false;
+	}
+	
+	//5.수정하기
+	public boolean boardUpdate(String title, String content, int seq){
+		int count=0;
+		SqlSession sqlSession=null;
+		
+		Map<String, Object>map=new HashMap<String, Object>();
+		map.put("title", title);
+		map.put("content", content);
+		map.put("seq", seq);
+		try {
+			// SessionFactory객체.openSessoin() -> SqlSession객체 얻어옴
+			// configuration.xml을 읽어서 생성된 객체
+			sqlSession=getSessionFactory().openSession(true);
+			//파라미터 타입: dto, Array, Map, String, Integer 등
+			count=sqlSession.update(namespace+"boardupdate",map);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return count>0?true:false;
+	}
+	//6.삭제하기
+	public boolean deleteBoard(int seq){
+		int count=0;
+		SqlSession sqlSession=null;
+		
+		try {
+			sqlSession=getSessionFactory().openSession(true);
+			//파라미터 타입: dto, Array, Map, String, Integer 등
+			count=sqlSession.update(namespace+"boarddelete",seq);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return count>0?true:false;
+	}
+	//7.여러글 삭제하기
+	public boolean mulDel(String[] seqs){
+		int count=0;
+		SqlSession sqlSession=null;
+		
+		//동적쿼리에 전달되는 파리미터는 Map에 담아서 전달한다.
+		Map<String, String[]>map=new HashMap<String, String[]>();
+		map.put("seqs", seqs);
+		
+		try {
+			sqlSession=getSessionFactory().openSession(true);
+			//파라미터 타입: dto, Array, Map, String, Integer 등
+			count=sqlSession.update(namespace+"muldel",map);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return count>0?true:false;
+	}
+}
---
+++ src/main/java/com/hk/board/test/AnsDaoTest.java
@@ -0,0 +1,18 @@
+package com.hk.board.test;
+
+import java.util.List;
+import com.hk.board.dao.AnsDao;
+import com.hk.board.dto.AnsDto;
+
+public class AnsDaoTest {
+
+	public static void main(String[] args) {
+		
+		AnsDao dao=new AnsDao();
+		List<AnsDto> list=dao.getAllList();		
+		
+		
+		System.out.println(list);
+	}
+
+}
